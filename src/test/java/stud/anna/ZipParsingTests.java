4073cc47ba3cf5661d8d703ec7bf7ff19066a4ba
Anna Petrova <mail.anna.p@yandex.ru>
2023-01-02 15:19:28 +0500

    add zipParseTest

--- src/test/java/stud/anna/ZipParsingTests.java
+++ src/test/java/stud/anna/ZipParsingTests.java
@@ -1,5 +1,46 @@
 package stud.anna;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class ZipParsingTests {
+
+    ClassLoader cl = ZipParsingTests.class.getClassLoader();
+
+    @Test
+    void zipParseTest() throws Exception {
+        try (
+                InputStream resource = cl.getResourceAsStream("Files.zip");
+                ZipInputStream zis = new ZipInputStream(resource)
+        ) {
+            ZipEntry entry;
+            while((entry = zis.getNextEntry()) != null) {
+                String name = entry.getName();
+
+                if (name.contains("Fond.xlsx")) {
+                    XLS content = new XLS(zis);
+                    assertThat(content.excel.getSheetAt(0).getRow(4).getCell(2).getStringCellValue()).contains("Гастроэнтерологическое");
+                } else if (name.contains("list.csv")) {
+                    CSVReader reader = new CSVReader(new InputStreamReader(zis));
+                    List<String[]> content = reader.readAll();
+                    assertThat(content.get(0)[1]).contains("number");
+                } else if (name.contains("Ticket.pdf")) {
+                    PDF Content = new PDF(zis);
+                    assertThat(Content.text).contains("KURGAN");
+                }
+
+            }
+        }
+    }
 
 }
